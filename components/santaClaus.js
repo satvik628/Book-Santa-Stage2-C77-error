d4995e3c3c6e344680fb8b470087bf71734b4073
satvik628 <71756957+satvik628@users.noreply.github.com>
2021-02-15 15:02:33 +0530

    Add files via upload

---
+++ components/santaClaus.js
@@ -0,0 +1,13 @@
+import *as React from 'react';
+import LottieView from 'lottie-react-native';
+
+export default class SantaClaus extends React.Component {
+  render() {
+    return (
+      <LottieView
+      source={require('../assets/41354-santa-claus.json')}
+      style={{width:"60%"}}
+      autoPlay loop />
+    )
+  }
+}
